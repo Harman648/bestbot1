099efb44961fb354932d4a8201d814fc3a723178
Harman648 <143630377+Harman648@users.noreply.github.com>
2023-09-03 21:09:26 +0530

    Delete Microsoft-Rewards-bot-master directory

--- Microsoft-Rewards-bot-master/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd %~dp0
-python ms_rewards_farmer.py --superfast --session --dont-check-for-updates --telegram 6032519992:AAGouXHWhmHF_4kOeelxdYgdN8g6mOHZ7Kg 1333143123 --currency INR
--- Microsoft-Rewards-bot-master/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 update.py --update
-python3 ms_rewards_farmer.py
